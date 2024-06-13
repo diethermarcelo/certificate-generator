92f6a3a3a07fcfdd76eb4ac2eb72fad2af85a296
Diether Marcelo <miguelitossss199@gmail.com>
2024-06-14 01:10:03 +0800

    Initial working generation

---
+++ README.md
@@ -0,0 +1,40 @@
+# certificate-generator
+- generate certificates by passing a json file
+
+## Requirements:
+- Python 3+
+
+## Support:
+- generate Microsoft Word
+
+## Setup:
+- 
+
+## How to use:
+1. Initially run the program to setup
+```shell
+python main.py || cd bin && generate-certificate
+```
+2. See the sample.json file
+3. Edit the data.json file 
+
+## Flow: 
+1. prompt, if first run then with setup, or if with params --setup
+2. choose either setup or generate:
+   1. setup
+      1. add field
+      2. edit field
+         1. loop then choose number to edit
+      3. remove field
+         1. loop choose number to remove
+      4. clear fields 
+   2. generate
+      1. read the data.json file 
+      2. get the template/template-word.docx
+      3. based on data there, loop through the data also based on accepted fields
+         1. field on the fields
+         2. save 
+
+## Preview
+
+
--- main.py
+++ main.py
@@ -1,1 +1,46 @@
-print("test")
+import docx
+import json
+
+def create_doc():
+    template_path = './template'
+    template_filename = 'template.docx'
+    template_fullpath = f"{template_path}/{template_filename}"
+    doc = docx.Document(template_fullpath)
+    return doc
+
+def replace_string(data):
+    doc = create_doc()
+    for p in doc.paragraphs:
+        terms = data.keys()
+        for term in terms:
+            if term in p.text:
+                replace_term = "{{" + term + "}}"
+                p.text = p.text.replace(replace_term, data[term])
+    return doc
+def save_doc(doc, data, increment):
+    current_file_name = data['file_name'] or "dest-{inc}".format(inc=increment)
+    file_name = "./dist/{current_file_name}.docx".format(current_file_name=current_file_name)
+    print("Creating {file_name}...".format(file_name=file_name))
+    doc.save(file_name)
+
+def main():
+    try:
+        f = open('./sample.json')
+        full_data = json.load(f)
+        i = 0
+        for data in full_data:
+            doc = replace_string(data)
+            save_doc(doc, data, i)
+            i += 1
+    except KeyError:
+        print('please input a file_name attribute on your sample.json file')
+    except:
+        print('problem occured. please try again later')
+    return 1
+
+if __name__ == '__main__':
+    main()
+
+
+
+
